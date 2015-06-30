51b6ec98f24aed2da3be43fde5fe5debc2547ddd
marcot <marco.teixeira@me.com.br>
2015-06-30 15:12:27 -0300

    Initial commit

---
+++ src/JogoBoliche.Core/Jogo.cs
@@ -0,0 +1,56 @@
+﻿namespace JogoBoliche.Core {
+	public class Jogo {
+		private const int PontuacaoSpare = 10;
+		private const int PontuacaoStrike = 10;
+
+		private int[] _arremessos = new int[21]; // Todos os arremessos, e um possível 3º arremesso no 10º frame
+		private int _arremessoAtual = 0;
+
+		public void Arremesso(int pinos) {
+			_arremessos[_arremessoAtual++] = pinos;
+		}
+
+		public int Pontuacao() {
+			const int totalFrames = 10;
+			var pontuacao = 0;
+			var frameIndex = 0;
+
+			for (var frame = 0; frame < totalFrames; frame++) {
+				if (Strike(frameIndex)) {
+					pontuacao += PontuacaoStrike + PontuacaoBonusStrike(frameIndex);
+					frameIndex++;
+				} else if (Spare(frameIndex)) {
+					pontuacao += PontuacaoSpare + PontuacaoBonusSpare(frameIndex);
+					frameIndex += 2;
+				} else {
+					pontuacao += SomarTotalPontuacaoFrame(frameIndex);
+					frameIndex += 2;
+				}
+			}
+
+			return pontuacao;
+		}
+
+		private bool Strike(int frameIndex) {
+			return _arremessos[frameIndex] == 10;
+		}
+
+		private int PontuacaoBonusStrike(int frameIndex) {
+			return _arremessos[frameIndex + 1] + _arremessos[frameIndex + 2];
+		}
+
+		private bool Spare(int frameIndex) {
+			return ((_arremessos[frameIndex] + _arremessos[frameIndex + 1]) == 10);
+		}
+
+		private int PontuacaoBonusSpare(int frameIndex) {
+			return _arremessos[frameIndex + 2];
+		}
+
+		private int SomarTotalPontuacaoFrame(int frameIndex) {
+			return _arremessos[frameIndex] + _arremessos[frameIndex + 1];
+		}
+
+
+	}
+}
---
+++ tests/JogoBoliche.UnitTest/JogoTests.cs
@@ -0,0 +1,88 @@
+﻿using JogoBoliche.Core;
+using NUnit.Framework;
+
+namespace JogoBoliche.UnitTest {
+	[TestFixture]
+	public class JogoTests {
+		private Jogo _jogo;
+
+		[SetUp]
+		public void SetUp() {
+			_jogo = new Jogo();
+		}
+
+		[TearDown]
+		public void TearDown() {
+			_jogo = null;
+		}
+
+		private void ExecutarArremessos(int vezes, int pinos) {
+			for (int arremesso = 0; arremesso < vezes; arremesso++) {
+				_jogo.Arremesso(pinos);
+			}
+		}
+
+		private void ArremessarSpare() {
+			_jogo.Arremesso(5);
+			_jogo.Arremesso(5);
+		}
+
+		[Test]
+		public void Jogadas_Na_Canaleta() {
+			// arrange
+			// Feito pelo método SetUp
+
+			// act
+			ExecutarArremessos(20, 0);
+
+			// assert
+			Assert.AreEqual(0, _jogo.Pontuacao());
+		}
+
+		[Test]
+		public void Todos_Arremessos_Apenas_Um_Pino() {
+			// arrange
+			// Feito pelo método SetUp
+
+			// act
+			ExecutarArremessos(20, 1);
+
+			// assert
+			Assert.AreEqual(20, _jogo.Pontuacao());
+		}
+
+		[Test]
+		public void Alcancando_Um_Spare() {
+			// arrange
+			// Feito pelo método SetUp
+
+			// act
+			ArremessarSpare();
+			_jogo.Arremesso(3);
+			ExecutarArremessos(17, 0); // Todos os próximos arremessos foram na canaleta. =(
+
+			// assert
+			Assert.AreEqual(16, _jogo.Pontuacao());
+		}
+
+		[Test]
+		public void Alcancando_Um_Strike() {
+			// arrange
+			// Feito pelo método SetUp
+
+			// act
+			_jogo.Arremesso(10);
+			_jogo.Arremesso(3);
+			_jogo.Arremesso(4);
+			ExecutarArremessos(16, 0); // Todos os próximos arremessos foram na canaleta. =(
+
+			// Frame 1 (Strike): 10 + 3 + 4 (17)
+			// Frame 2:1 (3): 3
+			// Frame 2:2 (4): 4
+			// Total: 24
+
+			// assert
+			Assert.AreEqual(24, _jogo.Pontuacao());
+		}
+	}
+}
